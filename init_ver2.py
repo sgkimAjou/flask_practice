f72f5d479b58a35e748a55a6b92412e0f8007782
jlkinspection <ypmn88@ajou.ac.kr>
2017-11-30 20:35:54 +0900

    sgkim

---
+++ init_ver2.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, redirect, url_for, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+
+#def index():
+#	return "Hello JLK Inspection"
+
+def login():
+	error=None
+	if request.method == 'POST':
+		if request.form['username']!='admin' or request.form['password']!='admin':
+			error = 'Try again'
+		else:
+			return redirect(url_for('secret'))
+	return render_template('index.html',error=error)
+
+@app.route('/secret')
+
+def secret():
+	return "Secret"
+
+
+
+if __name__=="__main__":
+	app.run()
+
